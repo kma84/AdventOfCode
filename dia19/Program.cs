46854d1567a5258bb508a69868d5f2a9e2760009
Sergio <sergio.kma@gmail.com>
2022-08-22 20:41:56 +0200

    Optimización de la parte 1 para detectar los puntos coincidentes mediante distancias entre puntos en lugar de coordenadas relativas.

--- dia19/Program.cs
+++ dia19/Program.cs
@@ -24,7 +24,7 @@ void Part1()
     foreach (Collision collision in scanners[0].Collisions.Where(c => !c.TargetScanner.Merged))
     {
         collision.TargetScanner.Merged = true;
-        resultPoints.UnionWith(GetPointsRelativeToA(scanners[0], collision.TargetScanner, collision.SourcePoint, collision.TargetPoint, collision.Rotation));
+        resultPoints.UnionWith(GetPointsRelativeToA(scanners[0], collision.TargetScanner, collision.SourcePoint, collision.TargetPoint));
     }
 
     Console.WriteLine("\nPart1: Number of beacons detected: " + resultPoints.Count);
@@ -35,59 +35,60 @@ void Part2()
 }
 
 
-List<(int x, int y, int z)> GetPointsRelativeToA(Scanner scannerA, Scanner scannerB, (int x, int y, int z) pointA, (int x, int y, int z) pointB, string rotation)
+List<(int x, int y, int z)> GetPointsRelativeToA(Scanner scannerA, Scanner scannerB, (int x, int y, int z) pointA, (int x, int y, int z) pointB)
 {
-    (int x, int y, int z) rotatedBPoint = RotatePoint(pointB, rotation);
-    (int x, int y, int z) scannerCoordsRelToA = (pointA.x - rotatedBPoint.x, pointA.y - rotatedBPoint.y, pointA.z - rotatedBPoint.z);
 
-    HashSet<(int x, int y, int z)> rotatedPoints = new(RotatePoints(scannerB.Beacons, rotation));
-    foreach (Collision collision in scannerB.Collisions.Where(c => !c.TargetScanner.Merged))
+    foreach (string rotation in Config.ROTATIONS)
     {
-        collision.TargetScanner.Merged = true;
-        var newpoints = GetPointsRelativeToA(scannerB, collision.TargetScanner, collision.SourcePoint, collision.TargetPoint, collision.Rotation);
-        rotatedPoints.UnionWith(RotatePoints(newpoints, rotation));
+        (int x, int y, int z) rotatedBPoint = RotatePoint(pointB, rotation);
+        (int x, int y, int z) scannerCoordsRelToA = (pointA.x - rotatedBPoint.x, pointA.y - rotatedBPoint.y, pointA.z - rotatedBPoint.z);
+
+        List<(int x, int y, int z)> lstRotatedPoints = new(RotatePoints(scannerB.Beacons, rotation));
+        List<(int x, int y, int z)> lstRotatedPointsRelToA = SumScannerCoords(lstRotatedPoints, scannerCoordsRelToA);
+
+        var intersection = scannerA.Beacons.Intersect(lstRotatedPointsRelToA);
+        if (intersection.Count() >= Config.MIN_OVERLAPPING_BEACONS - 1)
+        {
+            HashSet<(int x, int y, int z)> hSetRotatedPoints = new(lstRotatedPoints);
+
+            foreach (Collision collision in scannerB.Collisions.Where(c => !c.TargetScanner.Merged))
+            {
+                collision.TargetScanner.Merged = true;
+                var newpoints = GetPointsRelativeToA(scannerB, collision.TargetScanner, collision.SourcePoint, collision.TargetPoint);
+                hSetRotatedPoints.UnionWith(RotatePoints(newpoints, rotation));
+            }
+
+            return SumScannerCoords(hSetRotatedPoints, scannerCoordsRelToA);
+        }
     }
 
-    List<(int x, int y, int z)> lstRotatedPoints = rotatedPoints.ToList();
-
-    for (int i = 0; i < rotatedPoints.Count; i++)
-    {
-        lstRotatedPoints[i] = (
-            scannerCoordsRelToA.x + lstRotatedPoints[i].x,
-            scannerCoordsRelToA.y + lstRotatedPoints[i].y,
-            scannerCoordsRelToA.z + lstRotatedPoints[i].z
-        );
-    }
-
-    var intersection = scannerA.Beacons.Intersect(lstRotatedPoints);
-    if (intersection.Count() >= Config.MIN_OVERLAPPING_BEACONS - 1)
-    {
-        return lstRotatedPoints;
-    }
-
-    throw new Exception($"Collision not found. {scannerA}-{scannerB}, {rotation}");
+    throw new Exception($"Collision not found. {scannerA}-{scannerB}");
 }
+
+
+List<(int x, int y, int z)> SumScannerCoords(IEnumerable<(int x, int y, int z)> lstPoints, (int x, int y, int z) scannerCoords) => lstPoints.Select(p => (
+            scannerCoords.x + p.x,
+            scannerCoords.y + p.y,
+            scannerCoords.z + p.z
+        )).ToList();
 
 
 bool Intersect(Scanner scannerA, Scanner scannerB)
 {
-    for (int i = 0; i < scannerA.InterDistances.Count; i++)
+    for (int i = 0; i < scannerA.Distances.Count; i++)
     {
-        for (int j = 0; j < scannerB.InterDistances.Count; j++)
+        for (int j = 0; j < scannerB.Distances.Count; j++)
         {
-            foreach (var rotation in Config.ROTATIONS)
+            var intersection = scannerA.Distances[i].Intersect(scannerB.Distances[j]);
+
+            if (intersection.Count() >= Config.MIN_OVERLAPPING_BEACONS - 1)
             {
-                var result = scannerA.InterDistances[i].Intersect(RotatePoints(scannerB.InterDistances[j], rotation));
+                if (Config.debug)
+                    Console.WriteLine($"\n({scannerA},{scannerB}) - Overlapping beacons:{intersection.Count()}");
 
-                if (result.Count() >= Config.MIN_OVERLAPPING_BEACONS - 1)
-                {
-                    if (Config.debug)
-                        Console.WriteLine($"\n({scannerA},{scannerB}) - Overlapping beacons:{result.Count()} Rotation:({rotation})");
+                scannerA.Collisions.Add(new Collision(scannerA, scannerA.Beacons[i], scannerB, scannerB.Beacons[j]));
 
-                    scannerA.Collisions.Add(new Collision(scannerA, scannerA.Beacons[i], scannerB, scannerB.Beacons[j], rotation));
-
-                    return true;
-                }
+                return true;
             }
         }
     }
@@ -217,7 +218,7 @@ static class Config
         "(-z,-y,-x)"
     };
 
-    public static bool debug = true;
+    public static bool debug = false;
     public static string input = "input.txt"; // "input.txt" "inputTest.txt" "inputTest2.txt"
 }
 
@@ -230,41 +231,45 @@ class Scanner
 
     public List<(int x, int y, int z)> Beacons { get; set; } = new();
 
-    public List<List<(int x, int y, int z)>> InterDistances { get; set; } = new();
+    public List<List<double>> Distances { get; set; } = new();
 
     public List<Collision> Collisions { get; set; } = new();
 
     public Scanner(List<(int x, int y, int z)> beacons, int id)
     {
         Beacons = beacons;
-        InterDistances = GetInterDistances(beacons);
+        Distances = GetDistances(beacons);
         Id = id;
     }
 
-    private List<List<(int x, int y, int z)>> GetInterDistances(List<(int x, int y, int z)> points)
+
+    private static List<List<double>> GetDistances(List<(int x, int y, int z)> points)
     {
-        List<List<(int x, int y, int z)>> interDistances = new();
+        List<List<double>> distances = new();
 
         for (int i = 0; i < points.Count; i++)
         {
-            interDistances.Add(new List<(int x, int y, int z)>());
+            distances.Add(new List<double>());
 
             for (int j = 0; j < points.Count; j++)
             {
                 if (i == j)
                     continue;
 
-                interDistances[i].Add(GetPointsDiff(points[j], points[i]));
+                distances[i].Add(GetDistance(points[j], points[i]));
             }
         }
 
-        return interDistances;
+        return distances;
     }
 
-    private (int x, int y, int z) GetPointsDiff((int x, int y, int z) pointA, (int x, int y, int z) pointB)
+    private static double GetDistance((int x, int y, int z) pointA, (int x, int y, int z) pointB)
     {
-        return (pointB.x - pointA.x, pointB.y - pointA.y, pointB.z - pointA.z);
+        // Distance between two points in a three dimensional space
+        // d = ((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2)^1/2
+        return Math.Pow(Math.Pow(pointB.x - pointA.x, 2) + Math.Pow(pointB.y - pointA.y, 2) + Math.Pow(pointB.z - pointA.z, 2), 0.5);
     }
+
 
     public override string ToString() => $"Scanner{Id}";
 }
@@ -275,14 +280,12 @@ class Collision
     public Scanner SourceScanner { get; set; }
     public (int x, int y, int z) SourcePoint { get; set; }
     public (int x, int y, int z) TargetPoint { get; set; }
-    public string Rotation { get; set; }
 
-    public Collision(Scanner sourceScanner, (int x, int y, int z) sourcePoint, Scanner targetScanner, (int x, int y, int z) targetPoint, string rotation)
+    public Collision(Scanner sourceScanner, (int x, int y, int z) sourcePoint, Scanner targetScanner, (int x, int y, int z) targetPoint)
     {
         TargetScanner = targetScanner;
         SourceScanner = sourceScanner;
         SourcePoint = sourcePoint;
         TargetPoint = targetPoint;
-        Rotation = rotation;
     }
 }
