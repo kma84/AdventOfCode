579aa282822ccf332954a0b6cc0a2a3b834c24d7
Sergio <sergio.kma@gmail.com>
2022-08-31 23:54:13 +0200

    Day 20 Parts 1 and 2

--- AdventOfCode/Year2021/Day20/Problem.cs
+++ AdventOfCode/Year2021/Day20/Problem.cs
@@ -7,71 +7,134 @@ namespace AdventOfCode.Year2021.Day20
     [Problem(Year = 2021, Day = 20, ProblemName = "Trench Map")]
     internal class Problem : IProblem
     {
-        public bool Debug => true;
+        public bool Debug => false;
+
+        private const char DARK_PIXEL = '.';
+        private const char LIGHT_PIXEL = '#';
 
         public string Part1(string input)
         {
-            void PrintDebug(char[,] image)
-            {
-                //if (Debug)
-                    image.Print();
-            }
-
             (string enhancementAlgorithm, char[,] image) = GetDataFromInput(input);
             PrintDebug(image);
             
-            image = ResizeMatrix(image, 2);
-            PrintDebug(image);
-
-            for (int i = 0; i < 2; i++)
-            {
-                image = ProcessImage(image, enhancementAlgorithm);
-                image = ResizeMatrix(image, 1);
-
-                PrintDebug(image);
-            }
-            
-            return image.Count(c => c == '#').ToString();
+            return GetLitPixels(image, enhancementAlgorithm, iterations: 2).ToString();
         }
 
         public string Part2(string input)
         {
-			return "Part2";
+            (string enhancementAlgorithm, char[,] image) = GetDataFromInput(input);
+
+            return GetLitPixels(image, enhancementAlgorithm, iterations: 50).ToString();
         }
 
 
-        private static char[,] ProcessImage(char[,] image, string enhancementAlgorithm)
+        private int GetLitPixels(char[,] image, string enhancementAlgorithm, int iterations)
+        {
+            var infinitePixels = GetInfinitePixels(enhancementAlgorithm);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                image = ProcessImage(image, enhancementAlgorithm, infinitePixels[i % infinitePixels.Count]);
+            }
+
+            return image.Count(c => c == LIGHT_PIXEL);
+        }
+
+
+        private char[,] ProcessImage(char[,] image, string enhancementAlgorithm, char infinitePixel)
         {
             char[,] newImage = new char[image.GetLength(0), image.GetLength(1)];
-            newImage.Fill('.');
+            newImage.Fill(DARK_PIXEL);
 
-            for (int y = 1; y < image.GetLength(0) - 1; y++)
+            for (int y = 0; y < image.GetLength(0); y++)
             {
-                for (int x = 1; x < image.GetLength(1) - 1; x++)
-                {
-                    string pixels = GetPixels(image, x, y);
-
-                    char newPixel = enhancementAlgorithm[GetIndex(pixels)];
-                    newImage[y, x] = newPixel;
+                for (int x = 0; x < image.GetLength(1); x++)
+                {                    
+                    newImage[y, x] = ProcessPixel(image, x, y, enhancementAlgorithm, infinitePixel);
                 }
+            }
+
+            PrintDebug(newImage);
+
+            while (!FrameFormed(newImage))
+            {
+                image = ResizeMatrix(image, 1, infinitePixel);
+                newImage = ResizeMatrix(newImage, 1, infinitePixel);
+                char newPixel;
+
+                for (int y = 0; y < image.GetLength(0); y++)
+                {
+                    if (y == 0 || y == image.GetLength(0) - 1)
+                    {
+                        for (int x = 0; x < image.GetLength(1); x++)
+                        {
+                            newPixel = ProcessPixel(image, x, y, enhancementAlgorithm, infinitePixel);
+                            newImage[y, x] = newPixel;
+                        }
+                    }
+                    else
+                    {
+                        newPixel = ProcessPixel(image, 0, y, enhancementAlgorithm, infinitePixel);
+                        newImage[y, 0] = newPixel;
+
+                        newPixel = ProcessPixel(image, image.GetLength(1) - 1, y, enhancementAlgorithm, infinitePixel);
+                        newImage[y, image.GetLength(1) - 1] = newPixel;
+                    }
+                }
+
+                PrintDebug(newImage);
             }
 
             return newImage;
         }
 
-        private static string GetPixels(char[,] image, int x, int y)
+        private static bool FrameFormed(char[,] image)
+        {
+            char control = image[0, 0];
+
+            for (int y = 0; y < image.GetLength(0); y++)
+            {
+                if (y == 0 || y > image.GetLength(0) - 1)
+                {
+                    for (int x = 0; x < image.GetLength(1); x++)
+                    {
+                        if (image[y, x] != control)
+                            return false;
+                    }
+                }
+                else
+                {
+                    if (image[y, 0] != control)
+                        return false;
+
+                    if (image[y, image.GetLength(1) - 1] != control)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static char ProcessPixel(char[,] image, int x, int y, string enhancementAlgorithm, char infinitePixel)
+        {
+            string pixels = GetPixels(image, x, y, infinitePixel);
+
+            return enhancementAlgorithm[GetIndex(pixels)];
+        }
+
+        private static string GetPixels(char[,] image, int x, int y, char infinitePixel)
         {
             List<char> pixels = new()
             {
-                image[y - 1, x - 1],
-                image[y - 1, x],
-                image[y - 1, x + 1],
-                image[y, x - 1],
+                y - 1 >= 0 && x - 1 >= 0 ? image[y - 1, x - 1] : infinitePixel,
+                y - 1 >= 0 ? image[y - 1, x] : infinitePixel,
+                y - 1 >= 0 && x + 1 < image.GetLength(1) ? image[y - 1, x + 1] : infinitePixel,
+                x - 1 >= 0 ? image[y, x - 1] : infinitePixel,
                 image[y, x],
-                image[y, x + 1],
-                image[y + 1, x - 1],
-                image[y + 1, x],
-                image[y + 1, x + 1]
+                x + 1 < image.GetLength(1) ? image[y, x + 1] : infinitePixel,
+                y + 1 < image.GetLength(0) && x - 1 >= 0 ? image[y + 1, x - 1] : infinitePixel,
+                y + 1 < image.GetLength(0) ? image[y + 1, x] : infinitePixel,
+                y + 1 < image.GetLength(0) && x + 1 < image.GetLength(1) ? image[y + 1, x + 1] : infinitePixel
             };
 
             return new string(pixels.ToArray());
@@ -93,7 +156,7 @@ namespace AdventOfCode.Year2021.Day20
             return (enhancementAlgorithmString, image);
         }
 
-        private static char[,] ResizeMatrix(char[,] original, int offset)
+        private static char[,] ResizeMatrix(char[,] original, int offset, char infinitePixel)
         {
             int newYLength = original.GetLength(0) + offset * 2;
             int newXLength = original.GetLength(1) + offset * 2;
@@ -109,7 +172,7 @@ namespace AdventOfCode.Year2021.Day20
                     if (adjustedY >= 0 && adjustedY < original.GetLength(0) && adjustedX >= 0 && adjustedX < original.GetLength(1))
                         newArray[y, x] = original[adjustedY, adjustedX];
                     else
-                        newArray[y, x] = '.';
+                        newArray[y, x] = infinitePixel;
                 }
             }
 
@@ -118,8 +181,25 @@ namespace AdventOfCode.Year2021.Day20
 
         private static int GetIndex(string pixels)
         {
-            string pixelsBin = pixels.Replace('.', '0').Replace('#', '1');
+            string pixelsBin = pixels.Replace(DARK_PIXEL, '0').Replace(LIGHT_PIXEL, '1');
             return Convert.ToInt32(pixelsBin, 2);
+        }
+
+        private static List<char> GetInfinitePixels(string enhancementAlgorithm)
+        {
+            char firstPixel = enhancementAlgorithm[0];
+            List<char> infinitePixels = new List<char> { firstPixel };
+
+            if (firstPixel == LIGHT_PIXEL)
+                infinitePixels.Insert(0, enhancementAlgorithm.Last());
+
+            return infinitePixels;
+        }
+
+        private void PrintDebug(char[,] image)
+        {
+            if (Debug)
+                image.Print();
         }
 
     }
