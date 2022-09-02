12b66d0f1b9728b9bffa129db7be66f34d942f8b
Sergio <sergio.kma@gmail.com>
2022-09-03 00:22:15 +0200

    Day 4 in new format.

--- AdventOfCode.Utils/StringExtensions.cs
+++ AdventOfCode.Utils/StringExtensions.cs
@@ -4,7 +4,9 @@ namespace AdventOfCode.Utils
     public static class StringExtensions
     {
 
-        public static string[] GetLines(this string str) => str.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        public static string[] GetLines(this string str) => str.GetLines(StringSplitOptions.None);
+
+        public static string[] GetLines(this string str, StringSplitOptions options) => str.Split(new string[] { "\r\n", "\r", "\n" }, options);
 
     }
 }
--- AdventOfCode/Year2021/Day04/Problem.cs
+++ AdventOfCode/Year2021/Day04/Problem.cs
@@ -4,17 +4,191 @@ using AdventOfCode.Utils;
 
 namespace AdventOfCode.Year2021.Day04
 {
-    [Problem(Year = 2021, Day = 4, ProblemName = "")]
+    [Problem(Year = 2021, Day = 4, ProblemName = "Giant Squid")]
     internal class Problem : IProblem
     {
+        private static readonly int TAMAÑO_FILA_COLUMNA = 5;
+
+        public bool Debug { get; set; } = false;
+
+
         public string Part1(string input)
         {
-			return string.Empty;
+            (List<int> numeros, List<Casilla[,]> boards) = GetInputs(input);
+
+            return GetSolutionPart1(numeros, boards).ToString();
         }
 
         public string Part2(string input)
         {
-			return string.Empty;
+            (List<int> numeros, List<Casilla[,]> boards) = GetInputs(input);
+
+            return GetSolutionPart2(numeros, boards).ToString();
         }
-	}
+
+
+        private int GetSolutionPart1(List<int> numeros, List<Casilla[,]> boards)
+        {
+            foreach (int numero in numeros)
+            {
+                foreach (Casilla[,] board in boards)
+                {
+                    for (int i = 0; i < TAMAÑO_FILA_COLUMNA; i++)
+                    {
+                        for (int j = 0; j < TAMAÑO_FILA_COLUMNA; j++)
+                        {
+                            if (board[i, j].Numero == numero)
+                            {
+                                board[i, j].Marcado = true;
+
+                                if (ComprobarFila(board, i) || ComprobarColumna(board, j))
+                                {
+                                    return CalcularPuntuacion(board, numero);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+
+        private int GetSolutionPart2(List<int> numeros, List<Casilla[,]> casillas)
+        {
+            List<Board> boards = casillas.Select(c => new Board { Casillas = c, Marcado = false }).ToList();
+
+            foreach (int numero in numeros)
+            {
+                foreach (Board board in boards)
+                {
+                    for (int i = 0; i < TAMAÑO_FILA_COLUMNA; i++)
+                    {
+                        for (int j = 0; j < TAMAÑO_FILA_COLUMNA; j++)
+                        {
+                            if (board.Casillas != null && board.Casillas[i, j].Numero == numero)
+                            {
+                                board.Casillas[i, j].Marcado = true;
+
+                                if (ComprobarFila(board.Casillas, i) || ComprobarColumna(board.Casillas, j))
+                                {
+                                    board.Marcado = true;
+
+                                    if (boards.All(b => b.Marcado))
+                                    {
+                                        return CalcularPuntuacion(board.Casillas, numero);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+
+        private int CalcularPuntuacion(Casilla[,] board, int numero)
+        {
+            int sumaNumsNoMarcados = 0;
+
+            for (int i = 0; i < TAMAÑO_FILA_COLUMNA; i++)
+            {
+                for (int j = 0; j < TAMAÑO_FILA_COLUMNA; j++)
+                {
+                    if (!board[i, j].Marcado)
+                    {
+                        sumaNumsNoMarcados += board[i, j].Numero;
+                    }
+                }
+            }
+
+            int producto = sumaNumsNoMarcados * numero;
+
+            if (Debug)
+                Console.WriteLine($"Suma números no marcados: {sumaNumsNoMarcados}, último número: {numero}, producto: {producto}");
+
+            return producto;
+        }
+
+
+        private static (List<int> numeros, List<Casilla[,]> boards) GetInputs(string input)
+        {
+            string[] filas = input.GetLines(StringSplitOptions.RemoveEmptyEntries);
+
+            List<int> numeros = filas.First().Split(',').Select(n => int.Parse(n)).ToList();
+
+            Casilla[,] board = new Casilla[TAMAÑO_FILA_COLUMNA, TAMAÑO_FILA_COLUMNA];
+            int filaBoard = 0;
+            List<Casilla[,]> boards = new();
+
+            foreach (string fila in filas.Skip(1))
+            {
+                int[] numsFila = fila.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+
+                board[filaBoard, 0] = new Casilla { Numero = numsFila[0], Marcado = false };
+                board[filaBoard, 1] = new Casilla { Numero = numsFila[1], Marcado = false };
+                board[filaBoard, 2] = new Casilla { Numero = numsFila[2], Marcado = false };
+                board[filaBoard, 3] = new Casilla { Numero = numsFila[3], Marcado = false };
+                board[filaBoard, 4] = new Casilla { Numero = numsFila[4], Marcado = false };
+
+                if (++filaBoard == TAMAÑO_FILA_COLUMNA)
+                {
+                    boards.Add(board);
+                    board = new Casilla[TAMAÑO_FILA_COLUMNA, TAMAÑO_FILA_COLUMNA];
+                    filaBoard = 0;
+                }
+            }
+
+            return (numeros, boards);
+        }
+
+
+        public static Casilla[] GetColumn(Casilla[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                    .Select(x => matrix[x, columnNumber])
+                    .ToArray();
+        }
+
+
+        public static Casilla[] GetRow(Casilla[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                    .Select(x => matrix[rowNumber, x])
+                    .ToArray();
+        }
+
+
+        private static bool ComprobarFila(Casilla[,] matrix, int numFila)
+        {
+            Casilla[] fila = GetRow(matrix, numFila);
+
+            return fila.All(c => c.Marcado);
+        }
+
+
+        private static bool ComprobarColumna(Casilla[,] matrix, int numColumna)
+        {
+            Casilla[] columna = GetColumn(matrix, numColumna);
+
+            return columna.All(c => c.Marcado);
+        }
+
+
+        public class Casilla
+        {
+            public int Numero { get; set; }
+            public bool Marcado { get; set; }
+        }
+
+
+        public class Board
+        {
+            public Casilla[,]? Casillas { get; set; }
+            public bool Marcado { get; set; }
+        }
+    }
 }
