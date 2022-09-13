17ebfede0f65a5d7b93421dd4ff7658e6b9984c6
Sergio <sergio.kma@gmail.com>
2022-09-13 23:50:51 +0200

    Day 7 in new format.

---
+++ AdventOfCode/Year2021/Day07/Problem.cs
@@ -0,0 +1,84 @@
+using AdventOfCode.Core;
+using AdventOfCode.Core.Interfaces;
+
+namespace AdventOfCode.Year2021.Day07
+{
+    [Problem(Year = 2021, Day = 7, ProblemName = "The Treachery of Whales")]
+    internal class Problem : IProblem
+    {
+        public bool Debug { get; set; } = false;
+
+        public string Part1(string input)
+        {
+            List<int> positions = input.Split(',').Select(n => int.Parse(n)).ToList();
+            int maxDistancia = positions.Max();
+            ConsumoFuel[] aConsumos = new ConsumoFuel[maxDistancia + 1];
+
+            for (int d = 0; d <= maxDistancia; d++)
+            {
+                ConsumoFuel consumoDistanciaActual = new ConsumoFuel { DistanciaDestino = d };
+
+                foreach (int position in positions)
+                {
+                    consumoDistanciaActual.FuelTotal += Math.Abs(position - d);
+                }
+
+                aConsumos[d] = consumoDistanciaActual;
+            }
+
+            int fuelMinimo = aConsumos.Min(c => c.FuelTotal);
+
+            if (Debug)
+                Console.WriteLine($"Puzle 1: Fuel mínimo para alinear la posición: {fuelMinimo}");
+
+            return fuelMinimo.ToString();
+        }
+
+        public string Part2(string input)
+        {
+            List<int> positions = input.Split(',').Select(n => int.Parse(n)).ToList();
+            int maxDistancia = positions.Max();
+            ConsumoFuel[] aConsumos = new ConsumoFuel[maxDistancia + 1];
+
+            for (int d = 0; d <= maxDistancia; d++)
+            {
+                ConsumoFuel consumoDistanciaActual = new ConsumoFuel { DistanciaDestino = d };
+
+                foreach (int position in positions)
+                {
+                    int distanciaEntreInputYDistanciaActual = Math.Abs(position - d);
+                    consumoDistanciaActual.FuelTotal += CalcularConsumoFuel(distanciaEntreInputYDistanciaActual);
+                }
+
+                aConsumos[d] = consumoDistanciaActual;
+            }
+
+            int fuelMinimo = aConsumos.Min(c => c.FuelTotal);
+
+            if (Debug)
+                Console.WriteLine($"Puzle 2: Fuel mínimo para alinear la posición: {fuelMinimo}");
+            
+            return fuelMinimo.ToString();
+        }
+
+
+        private static int CalcularConsumoFuel(int distancia)
+        {
+            int fuel = 0;
+
+            for (int d = 1; d <= distancia; d++)
+            {
+                fuel += d;
+            }
+
+            return fuel;
+        }
+
+
+        public class ConsumoFuel
+        {
+            public int DistanciaDestino { get; set; }
+            public int FuelTotal { get; set; }
+        }
+    }
+}
