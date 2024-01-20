916b03ec8ac7c7478d394c1cba0a8aa0ebcc600e
Sergio <sergio.kma@gmail.com>
2024-01-20 20:29:44 +0100

    Refactor 2022 day 13

--- AdventOfCode/Year2022/Day13/Problem.cs
+++ AdventOfCode/Year2022/Day13/Problem.cs
@@ -9,22 +9,22 @@ namespace AdventOfCode.Year2022.Day13
     {
         public bool Debug => false;
 
-        public string Part1(string input)
-        {
-            List<Pair> pairs = GetPairs(input.GetLines());
-
-            foreach (Pair pair in pairs)
-            {
-                pair.RightOrder = CompareNodes(pair.LeftNode, pair.RightNode);
-            }
-
-			return pairs.Where(p => p.RightOrder ?? false).Sum(p => p.Index).ToString();
-        }
+        public string Part1(string input) => GetPairs(input.GetLines())
+            .Where(p => IsInRightOrder(p.LeftNode, p.RightNode) ?? false)
+            .Sum(p => p.Index)
+            .ToString();
 
         public string Part2(string input)
         {
+            string separatorNode1 = "[[2]]";
+            string separatorNode2 = "[[6]]";
+
             List<Node> nodes = GetNodes(input.GetLines());
-            List<Node> orderedNodes = new() { CreateNodeFromLine("[[2]]", true), CreateNodeFromLine("[[6]]", true) };
+            List<Node> orderedNodes = new()
+            {
+                CreateNodeFromLine(separatorNode1, isSeparatorNode: true),
+                CreateNodeFromLine(separatorNode2, isSeparatorNode: true)
+            };
 
             foreach (Node nodeToOrder in nodes)
             {
@@ -32,7 +32,7 @@ namespace AdventOfCode.Year2022.Day13
 
                 for (int i = 0; i < orderedNodes.Count; i++)
                 {
-                    bool? result = CompareNodes(nodeToOrder, orderedNodes[i]);
+                    bool? result = IsInRightOrder(nodeToOrder, orderedNodes[i]);
                     if (result ?? false)
                     {
                         orderedNodes.Insert(i, nodeToOrder);
@@ -42,72 +42,28 @@ namespace AdventOfCode.Year2022.Day13
                 }
 
                 if (!inserted)
-                {
                     orderedNodes.Add(nodeToOrder);
-                }
             }
 
-            return orderedNodes.Where(n => ((ListNode)n).IsSeparatorNode).Select(n => orderedNodes.IndexOf(n) + 1).Aggregate((total, next) => total * next).ToString();
+            return orderedNodes.Where(n => n.IsSeparatorNode)
+                               .Select(n => orderedNodes.IndexOf(n) + 1)
+                               .Aggregate((total, next) => total * next)
+                               .ToString();
         }
 
 
-
-        private bool? CompareNodes(Node leftNode, Node rightNode)
+        private static List<Node> GetNodes(string[] lines)
         {
-            if (leftNode is NumericNode lnode && rightNode is NumericNode rnode)
-            {
-                if (lnode.Value == rnode.Value)
-                {
-                    return null;
-                }
-                else
-                    return lnode.Value < rnode.Value;
-            }
-            else if (leftNode is NumericNode lnode2 && rightNode is ListNode)
-            {
-                return CompareNodes(new ListNode(lnode2.Value), rightNode);
-            }
-            else if (leftNode is ListNode && rightNode is NumericNode rnode2)
-            {
-                return CompareNodes(leftNode, new ListNode(rnode2.Value));
-            }
-            else if (leftNode is ListNode llistnode && rightNode is ListNode rlistnode)
-            {
-                int i = 0;
-                Node? lsubnode;
-                Node? rsubnode;
+            string[] linesFiltered = lines.Where(l => !string.IsNullOrEmpty(l)).ToArray();
+            List<Node> nodes = new();
 
-                do
-                {
-                    lsubnode = llistnode.SubNodes.ElementAtOrDefault(i);
-                    rsubnode = rlistnode.SubNodes.ElementAtOrDefault(i);
+            foreach (string line in linesFiltered)
+                nodes.Add(CreateNodeFromLine(line));
 
-                    if (lsubnode == null && rsubnode != null)
-                    {
-                        return true;
-                    }
-                    if (lsubnode != null && rsubnode == null)
-                    {
-                        return false;
-                    }
-
-                    bool? result = CompareNodes(lsubnode, rsubnode);
-
-                    if (result != null)
-                    {
-                        return result;
-                    }
-
-                    i++;
-                } while (lsubnode != null && rsubnode != null);
-            }
-
-            return null;
+            return nodes;
         }
 
-
-        // TODO ver parser del 2021 18
-        private static List<Pair> GetPairs(string[] lines)
+        private List<Pair> GetPairs(string[] lines)
         {
             List<Pair> pairs = new();
             int i = 0;
@@ -117,9 +73,12 @@ namespace AdventOfCode.Year2022.Day13
                 Node leftNode = CreateNodeFromLine(lines[i++]);
                 Node rightNode = CreateNodeFromLine(lines[i++]);
 
-                //Console.WriteLine(leftNode);
-                //Console.WriteLine(rightNode);
-                //Console.WriteLine();
+                if (Debug)
+                {
+                    Console.WriteLine(leftNode);
+                    Console.WriteLine(rightNode);
+                    Console.WriteLine();
+                }
 
                 pairs.Add(new Pair(pairs.Count + 1, leftNode, rightNode));
                 i++;
@@ -128,121 +87,113 @@ namespace AdventOfCode.Year2022.Day13
             return pairs;
         }
 
-        private static List<Node> GetNodes(string[] lines)
+        private static Node CreateNodeFromLine(string line, bool isSeparatorNode = false)
         {
-            string[] linesFiltered = lines.Where(l => !string.IsNullOrEmpty(l)).ToArray();
-            List<Node> nodes = new();
-
-            foreach (string line in linesFiltered)
-            {
-                nodes.Add(CreateNodeFromLine(line));
-            }
-
-            return nodes;
+            int index = 1;
+            return CreateNodeFromLine(line, ref index, isSeparatorNode);
         }
 
-        private static Node CreateNodeFromLine(string line, bool isSeparatorNode = false) 
+        private static Node CreateNodeFromLine(string line, ref int index, bool isSeparatorNode = false)
         {
-            ListNode listNode = new (isSeparatorNode);
-            Stack<char> stack = new ();
+            Node node = new(isSeparatorNode);
 
-            for (int i = 1; i < line.Length; i++)
+            while (index < line.Length)
             {
-                char c = line[i];
-                if (char.IsNumber(c))
-                {   
-                    string numStr = c.ToString() + (char.IsNumber(line[i+1]) ? line[i + 1].ToString() : string.Empty);
-                    listNode.SubNodes.Add(new NumericNode(int.Parse(numStr)));
-                    if (char.IsNumber(line[i + 1]))
-                    {
-                        i++;
-                    }
-                }
-                else if(c == '[')
+                char c = line[index];
+
+                switch (c)
                 {
-                    listNode.SubNodes.Add(CreateNodeFromLine(line[i..]));
-                    stack.Push(c);
+                    case char numberChar when char.IsNumber(numberChar):
+                        node.SubNodes.Add(new() { Value = GetNodeValue(c, line[index + 1], ref index) });
+                        break;
+                    case START_NODE_CHAR:
+                        index++;
+                        node.SubNodes.Add(CreateNodeFromLine(line, ref index));
+                        break;
+                    case END_NODE_CHAR:
+                        return node;
+                }
 
-                    while (stack.Any())
-                    {
-                        i++;
-                        char c2 = line[i];
-                        if (c2 == '[')
-                            stack.Push(c2);
-                        else if(c2 == ']')
-                            stack.Pop();
-                    }
-                }
-                else if (c == ']')
-                {
-                    return listNode;
-                }
+                index++;
             }
 
-            return listNode;
+            return node;
         }
 
-        private class Pair
+        private static int GetNodeValue(char c1, char c2, ref int index)
         {
-            public int Index { get; set; }
-            public bool? RightOrder { get; set; }
+            string numStr = c1.ToString();
 
-            public Node LeftNode { get; set; }
-            public Node RightNode { get; set; }
-
-            public Pair(int index, Node leftNode, Node rightNode)
+            if (char.IsNumber(c2))
             {
-                Index = index;
-                LeftNode = leftNode;
-                RightNode = rightNode;
+                numStr += c2.ToString();
+                index++;
             }
 
+            return int.Parse(numStr);
         }
+
+        private static bool? IsInRightOrder(Node? leftNode, Node? rightNode) => (leftNode, rightNode) switch
+        {
+            (null, null) => null,
+            (Node l, Node r) when l.Value.HasValue && r.Value.HasValue && l.Value == r.Value => null,
+            (Node l, Node r) when l.Value.HasValue && r.Value.HasValue => l.Value < r.Value,
+            (Node l, Node r) when l.Value.HasValue && !r.Value.HasValue => IsInRightOrder(new Node(l.Value.Value), r),
+            (Node l, Node r) when !l.Value.HasValue && r.Value.HasValue => IsInRightOrder(l, new Node(r.Value.Value)),
+            _ => CheckSubnodesOrder(leftNode, rightNode),
+        };
+
+        private static bool? CheckSubnodesOrder(Node? leftNode, Node? rightNode)
+        {
+            int i = 0;
+            Node? lsubnode;
+            Node? rsubnode;
+
+            do
+            {
+                lsubnode = leftNode?.SubNodes.ElementAtOrDefault(i);
+                rsubnode = rightNode?.SubNodes.ElementAtOrDefault(i);
+
+                if (lsubnode == null && rsubnode != null)
+                    return true;
+
+                if (lsubnode != null && rsubnode == null)
+                    return false;
+
+                bool? result = IsInRightOrder(lsubnode, rsubnode);
+
+                if (result != null)
+                    return result;
+
+                i++;
+            } while (lsubnode != null && rsubnode != null);
+
+            return null;
+        }
+
+
+        private const char START_NODE_CHAR = '[';
+        private const char END_NODE_CHAR = ']';
+
+        private record Pair(int Index, Node LeftNode, Node RightNode);
 
         private class Node
         {
-        }
+            public int? Value { get; set; }
+            public bool IsSeparatorNode { get; set; }
+            public List<Node> SubNodes { get; set; } = new();
 
-        private class NumericNode : Node
-        {
-            public int Value { get; set; }
-
-            public NumericNode(int value)
+            public Node(bool isSeparatorNode = false)
             {
-                Value = value;
+                IsSeparatorNode = isSeparatorNode;
             }
 
-            public override string ToString() => Value.ToString();
-        }
-
-        private class ListNode : Node
-        {
-            public List<Node> SubNodes { get; set; } = new ();
-            public bool IsSeparatorNode { get; set; } = false;
-
-            public ListNode(bool isSeparatorNode)
+            public Node(int subnodeValue)
             {
-                IsSeparatorNode= isSeparatorNode;
-            }
-            public ListNode(int value)
-            {
-                SubNodes.Add(new NumericNode(value));
+                SubNodes.Add(new Node() { Value = subnodeValue });
             }
 
-            public override string ToString()
-            {
-                string str = "[";
-
-                foreach (Node node in SubNodes)
-                {
-                    str += node.ToString() + ',';
-                }
-
-
-                str = str.TrimEnd(',');
-
-                return str + "]";
-            }
+            public override string ToString() => Value.HasValue ? Value.Value.ToString() : START_NODE_CHAR + string.Join(",", SubNodes) + END_NODE_CHAR;
         }
     }
 }
